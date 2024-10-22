efd751f9a091463f069c9ca9ee1813647c355079
shubham mokal <shubham@gmail.com>
2024-10-22 22:28:41 +0530

    CURd operation complete

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from app import views  # Import the views module
 
 urlpatterns = [
-    path('', views.home, name='home'),  
+    path('', views.home, name='home'),
+    path("insert",views.insertData,name="InsertData"),
+    path("update/<id>",views.UpdateData,name="UpdateData"),
+    path("delete/<id>",views.DeleteData,name="DeleteData"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render  # Make sure to import render
-
+from django.shortcuts import render  ,redirect# Make sure to import render
+from .models import Student
 def home(request):
-    return render(request, 'index.html')  # Render the 'index.html' template
+    
+    data=Student.objects.all()
+    context={"data":data}
+    return render(request, 'index.html',context)  # Render the 'index.html' template
+
+
+def insertData(request):
+    
+   
+    
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        age=request.POST.get("age")
+        gender=request.POST.get("gender")
+        print(name,age,gender,email)
+        
+        query=Student(name=name,email=email,age=age,gender=gender)
+        query.save()
+    return render(request,"index.html")
+
+
+def DeleteData(request,id):
+    
+    data=Student.objects.all()
+    context={"data":data}
+    return render(request, 'index.html',context)
+def UpdateData(request,id):
+    
+    
+    
+    if request.method=="POST":
+        name=request.POST["name"]
+        email=request.POST["email"]
+        age=request.POST["age"]
+        gender=request.POST["gender"]
+        print(name,age,gender,email)
+        
+        query=Student(name=name,email=email,age=age,gender=gender)
+        query.save()
+        d=Student.objects.get(id=id)g
+        context={"d":d}
+        
+        return redirect("/")
+        
+    return render(request, 'edit.html',context)
